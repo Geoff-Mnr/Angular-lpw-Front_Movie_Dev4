62c0a9cc303670f2e96b063ac5a2c263eb4f6b8c
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-03-10 14:19:22 +0100

    update add-edit-form and movie service

--- src/app/components/list-movies/list-movies.component.ts
+++ src/app/components/list-movies/list-movies.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { MovieService } from '../../services/movie.service';
-import { Movie } from '../../models/movie.interface';
-import { RouterLink } from '@angular/router';
-import { CommonModule, DatePipe } from '@angular/common';
-import { AddEditFormComponent } from '../add-edit-form/add-edit-form.component';
-import { Subscription } from 'rxjs';
-import { OnDestroy } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { MovieService } from "../../services/movie.service";
+import { Movie } from "../../models/movie.interface";
+import { RouterLink } from "@angular/router";
+import { CommonModule, DatePipe } from "@angular/common";
+import { AddEditFormComponent } from "../add-edit-form/add-edit-form.component";
+import { Subscription } from "rxjs";
+import { OnDestroy } from "@angular/core";
 
 @Component({
   selector: "app-list-movies",
@@ -15,75 +15,73 @@ import { OnDestroy } from '@angular/core';
   styleUrl: "./list-movies.component.scss",
 })
 export class ListMoviesComponent {
+  service = inject(MovieService);
+  selectedMovie?: Movie;
+  private subDelete: Subscription | undefined;
 
-service = inject(MovieService);
-selectedMovie?: Movie;
-private subscription: Subscription;
+  movies: Movie[] = [];
 
-movies: Movie[] = [];
+  displayForm = false;
 
-displayForm = false;
+  ngOnInit() {
+    this.getAllMovies();
+  }
 
-ngOnInit() {
-  this.getAllMovies();
-}
+  getAllMovies() {
+    this.subDelete = this.service.getMovies().subscribe({
+      next: (response) => {
+        this.movies = response;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 
-getAllMovies() {
-  this.subscription = this.service.getMovies().subscribe({
-    next: (response) => {
-      this.movies = response;
-    },
-    error: (error) => {
-      console.error(error);
-    },
-  });
-
-  selectedMovie(item: Movie){
+  selectMovie(item: Movie) {
     this.selectedMovie = item;
     console.log(this.selectedMovie);
-}
+  }
 
-updateMovie(item: Movie) {
-  this.subDelete = this.service.updateMovie(item.id, item).subscribe({
-    next: () => {
-      console.log('Movie updated successfully');
-      this.getAllMovies();
-      this.closeEditForm();
-    },
-    error: (error) => {
-      console.error('Error updating movie:', error);
-    }
-  });
-}
+  updateMovie(item: Movie) {
+    this.subDelete = this.service.updateMovie(item).subscribe({
+      next: () => {
+        console.log("Movie updated successfully");
+        this.getAllMovies();
+        this.closeEditForm();
+      },
+      error: (error) => {
+        console.error("Error updating movie:", error);
+      },
+    });
+  }
 
-createMovie(item: Movie) {
-  this.subDelete = this.service.createMovie(item).subscribe({
-    next: () => {
-      console.log("Movie created successfully");
-      this.getAllMovies();
-      this.closeAddForm();
-      this.closeEditForm();
-    },
-    error: (error) => {
-      console.error("Error creating movie:", error);
-    },
-  });
-}
-
+  createMovie(item: Movie) {
+    this.subDelete = this.service.addMovie(item).subscribe({
+      next: () => {
+        console.log("Movie created successfully");
+        this.getAllMovies();
+        this.closeAddForm();
+        this.closeEditForm();
+      },
+      error: (error) => {
+        console.error("Error creating movie:", error);
+      },
+    });
+  }
 
   // Méthode pour supprimer un film
   deleteMovie(item: Movie) {
-    this.subscription = this.service.deleteMovie(item.id).subscribe({
+    this.subDelete = this.service.deleteMovie(item.id).subscribe({
       next: () => {
         console.log("Movie deleted successfully'");
         this.getAllMovies();
       },
       error: (error) => {
-        console.error('Error deleting movie:', error);
+        console.error("Error deleting movie:", error);
       },
     });
   }
-
 
   private closeAddForm() {
     this.displayForm = false;
@@ -95,7 +93,8 @@ createMovie(item: Movie) {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.subDelete) {
+      this.subDelete.unsubscribe();
+    }
   }
-
 }
