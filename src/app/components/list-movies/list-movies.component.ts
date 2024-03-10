6d38441df101a97581a87f33df342fb02e4193f7
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-03-10 14:04:19 +0100

    update list-movies

--- src/app/components/list-movies/list-movies.component.ts
+++ src/app/components/list-movies/list-movies.component.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../models/movie.interface';
+import { RouterLink } from '@angular/router';
+import { CommonModule, DatePipe } from '@angular/common';
+import { AddEditComponent } from '../add-edit/add-edit.component';
+import { Subscription } from 'rxjs';
+import { OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'app-list-movies',
+  selector: "app-list-movies",
   standalone: true,
-  imports: [],
-  templateUrl: './list-movies.component.html',
-  styleUrl: './list-movies.component.scss'
+  imports: [RouterLink, CommonModule, DatePipe],
+  templateUrl: "./list-movies.component.html",
+  styleUrl: "./list-movies.component.scss",
 })
 export class ListMoviesComponent {
+
+service = inject(MovieService);
+selectedMovie?: Movie;
+private subscription: Subscription;
+
+movies: Movie[] = [];
+
+displayForm = false;
+
+ngOnInit() {
+  this.getAllMovies();
+}
+
+getAllMovies() {
+  this.subscription = this.service.getMovies().subscribe({
+    next: (response) => {
+      this.movies = response;
+    },
+    error: (error) => {
+      console.error(error);
+    },
+  });
+
+  selectedMovie(item: Movie){
+    this.selectedMovie = item;
+    console.log(this.selectedMovie);
+}
+
+updateMovie(item: Movie) {
+  this.subDelete = this.service.updateMovie(item.id, item).subscribe({
+    next: () => {
+      console.log('Movie updated successfully');
+      this.getAllMovies();
+      this.closeEditForm();
+    },
+    error: (error) => {
+      console.error('Error updating movie:', error);
+    }
+  });
+}
+
+createMovie(item: Movie) {
+  this.subDelete = this.service.createMovie(item).subscribe({
+    next: () => {
+      console.log("Movie created successfully");
+      this.getAllMovies();
+      this.closeAddForm();
+      this.closeEditForm();
+    },
+    error: (error) => {
+      console.error("Error creating movie:", error);
+    },
+  });
+}
+
+
+  // Méthode pour supprimer un film
+  deleteMovie(item: Movie) {
+    this.subscription = this.service.deleteMovie(item.id).subscribe({
+      next: () => {
+        console.log("Movie deleted successfully'");
+        this.getAllMovies();
+      },
+      error: (error) => {
+        console.error('Error deleting movie:', error);
+      },
+    });
+  }
+
+
+  private closeAddForm() {
+    this.displayForm = false;
+  }
+
+  /*Methode pour fermer le formulaire de modification*/
+  private closeEditForm() {
+    this.selectedMovie = undefined;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
