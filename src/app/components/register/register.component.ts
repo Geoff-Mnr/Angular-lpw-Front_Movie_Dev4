edbec63941c8b4eb281518648b0e08d193b0e5b8
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-03-22 13:10:16 +0100

    update

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { ToastrService } from "ngx-toastr";
 import { nospaceValidator } from "../../validators/nospace";
+import { ValidatorFn, ValidationErrors } from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -23,23 +24,39 @@ export class RegisterComponent {
   // Injection du service ToastrService
   toaster = inject(ToastrService);
 
+  passwordMatch: ValidatorFn = (control) => {
+    const password = control.get("password");
+    const confirm_password = control.get("confirm_password");
+
+    // Si les deux champs sont remplis et que les valeurs sont différentes
+    if (password && confirm_password && password.value !== confirm_password.value) {
+      if (confirm_password) {
+        confirm_password.setErrors({ passwordMatch: true });
+      }
+      // Sinon, on réinitialise les erreurs
+    } else {
+      if (confirm_password) {
+        confirm_password.setErrors(null);
+      }
+    }
+    return null;
+  };
+
   // Création du formulaire d'inscription
-  registerForm = new FormGroup({
-    email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", [Validators.required, Validators.minLength(8), nospaceValidator()]),
-    confirm_password: new FormControl("", [Validators.required, Validators.minLength(8), nospaceValidator()]),
-  });
+  registerForm = new FormGroup(
+    {
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8), nospaceValidator()]),
+      confirm_password: new FormControl("", [Validators.required, Validators.minLength(8), nospaceValidator()]),
+    },
+    { validators: this.passwordMatch }
+  );
 
   // Méthode appelée lors de la soumission du formulaire
   register() {
     let email = this.registerForm.value.email;
     let password = this.registerForm.value.password;
     let confirm_password = this.registerForm.value.confirm_password;
-
-    if (password !== confirm_password) {
-      this.toaster.error("Les mots de passe ne correspondent pas.", "Erreur");
-      return;
-    }
 
     if (email && password && confirm_password) {
       this.authService.register(email, password, confirm_password).subscribe({
